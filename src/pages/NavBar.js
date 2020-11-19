42431f42ff909ede564ff529f85d6c769ac46f6c
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2020-11-19 21:31:39 +0100

    deleted searchbar nav

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -7,13 +7,9 @@ export default function NavBar() {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">WELCOME</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/about">about</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/discover">Discover</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
       </Navbar>
       <br />
     </div>
